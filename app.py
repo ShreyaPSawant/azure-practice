03524029cf8cbb04d036484ddcbf00b1558cb9b3
shreya sawant <spsawant31@gmail.com>
2024-10-14 17:15:35 +0530

    added environment variable

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Azure!"
+    # Access the environment variable
+    my_variable = os.environ.get('MY_VARIABLE', 'Default Value')
+    return f"Value of MY_VARIABLE is: {my_variable}"
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', port=8000)
