47be9e95a88c2e35f7509ec43c5c733452a16c13
shreya sawant <spsawant31@gmail.com>
2024-10-15 12:09:52 +0530

    slot1 done

--- app.py
+++ app.py
@@ -5,8 +5,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    # Access the environment variable
-    my_variable = os.getenv('my_variable')
     return f"this is from slot 1"
 
 if __name__ == "__main__":
