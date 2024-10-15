8d6640cd6c89012f0451565ffe8e9fbdb712fedf
shreya sawant <spsawant31@gmail.com>
2024-10-15 16:12:12 +0530

    updating slot1.2

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return f"this is from slot 1 updated. pleaseeeeeeeeeeeeeeeee"
+    return f"this is from slot 1 updated. pleaseeeeeeeeeeeeeeeeeyyyyyy"
 
 if __name__ == "__main__":
     app.run()
